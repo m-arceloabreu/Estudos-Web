979fba4608bb6c61b1b58ce6775771c9ec750f3c
Marcelo Abreu <marceloabreu1997@gmail.com>
2021-07-31 12:48:20 -0300

    Update objetos.js

---
+++ Fundamentos/objetos.js
@@ -0,0 +1,14 @@
+//Objeto é uma coleção de chave e valor
+
+const prod1 = {}                //padrao pra objetos
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto legal'] = 0.4              //evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+}
+console.log(prod2)
